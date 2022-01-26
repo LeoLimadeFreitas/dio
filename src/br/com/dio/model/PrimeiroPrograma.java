0fca8ace3415e2645688be45d68a1f82860d1055
leonardo <leonardo.sud.freitas@gmail.com>
2022-01-26 16:02:53 -0600

    first commit

---
+++ src/br/com/dio/model/PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+package br.com.dio.model;
+
+import br.com.dio.model.*;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		
+		Livros livros = new Livros ();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+	
+}
