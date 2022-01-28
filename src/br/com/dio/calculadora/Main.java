2322def947be94762bb50022d10c03d4de22ae59
leonardo <leonardo.sud.freitas@gmail.com>
2022-01-28 15:48:08 -0600

    Calculadora

---
+++ src/br/com/dio/calculadora/Main.java
@@ -0,0 +1,36 @@
+package br.com.dio.calculadora;
+
+/*
+ * Classe de exemplo para o exercicio da aula 1 de Metodos.
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Calculadora
+		System.out.println("Exerc�cio calculadora");
+		Calculadora.soma(3, 6);
+		Calculadora.subtracao(9, 1.8);
+		Calculadora.multiplicacao(7, 8);
+		Calculadora.divisao(5, 2.5);
+		
+		
+		//Mensagem
+		
+		System.out.println("Exerc�cio mensagem");
+		Mensagem.obterMensagem(9);
+		Mensagem.obterMensagem(14);
+		Mensagem.obterMensagem(1);
+		
+		//Empr�stimo
+		
+		System.out.println("Exerc�cio empr�stimo");
+		Emprestimo.calcula(1000, Emprestimo.getDuasParcelas());
+		Emprestimo.calcula(1000, Emprestimo.getTresParcelas());
+		Emprestimo.calcula(1000,  5);
+		
+		
+	}
+
+}
