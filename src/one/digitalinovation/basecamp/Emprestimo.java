6797e78edca9f0ef4d554cbcb7883fe3d5114752
leonardo <leonardo.sud.freitas@gmail.com>
2022-01-28 15:44:59 -0600

    Calculadora

---
+++ src/one/digitalinovation/basecamp/Emprestimo.java
@@ -0,0 +1,51 @@
+package  one.digitalinovation.basecamp;
+
+/*
+ * Classe de exemplo para o exercicio da aula 1 de Metodos.
+ */
+
+public class Emprestimo {
+
+	public static int getDuasParcelas() {
+		
+		return 2;
+	}
+	
+	public static int getTresParcelas() {
+		
+		return 3;
+	}
+
+	public static double getTaxaDuasParcelas() {
+		
+		return  0.3;
+	}
+	
+	public static double getTaxaTresParcelas() {
+		
+		return  0.45;
+	}
+	
+	public static void calcula (double valor, int parcelas) {
+		
+		if (parcelas ==2) {
+			
+			double valorFinal = valor + (valor * getTaxaDuasParcelas());
+			
+			System.out.println("Valor final do empr�stimo para 2 parcelas: R$ " + valorFinal);
+			
+			
+		}else if (parcelas == 3) {
+			
+			double valorFinal = valor + ( valor * getTaxaTresParcelas());
+		
+			System.out.println("Valor final do empr�stimo para 3 parcelas: R$ " + valorFinal);
+			
+			
+		}else {
+			System.out.println("Quantidade de parcelas nao aceita.");
+			
+		}
+	}
+	
+}
